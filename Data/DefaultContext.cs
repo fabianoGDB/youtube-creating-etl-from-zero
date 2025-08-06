cb9bb4830b689ae3b9646af929d917ca74ba467e
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-05 23:38:28 -0300

    A004: creating dbcontext

--- Data/DefaultContext.cs
+++ Data/DefaultContext.cs
@@ -8,6 +8,15 @@ namespace youtube_etl_do_zero.Data
 {
     public class DefaultContext : DbContext
     {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
