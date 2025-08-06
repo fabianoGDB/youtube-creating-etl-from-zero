672685eb1f1b3107b23562cbad046bf264bc6fd9
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-05 23:55:48 -0300

    A009: adding albums model

---
+++ Models/Albums.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace youtube_etl_do_zero.Models
+{
+    public class Albums
+    {
+        [Key]
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+        [JsonPropertyName("userId")]
+        public long UserId { get; set; }
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+    }
+}
