779b8f426c3661d0722d46665dfeb0fcec2c7278
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-06 23:02:02 -0300

    A014: adding dbsets to the DefaultContext file

--- Data/DefaultContext.cs
+++ Data/DefaultContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using youtube_etl_do_zero.Models;
 
 namespace youtube_etl_do_zero.Data
 {
@@ -13,6 +14,13 @@ namespace youtube_etl_do_zero.Data
 
         }
 
+        public DbSet<Albums> Albums { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Photos> Photos { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Todos> Todos { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Address> Address { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
