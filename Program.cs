a22925e306ca7b3c7700d677cbdb136ecbbfe1fa
fabianoGDB <doctorfabi.f@gmail.com>
2025-08-06 23:35:12 -0300

    A016: adding configuration for the application run

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using youtube_etl_do_zero.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("MariaDBContext");
+
+builder.Services.AddControllers()
+    .AddJsonOptions(x =>
+    {
+        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
+builder.Services.AddDbContext<DefaultContext>(options =>
+    options.UseMySql(connString, ServerVersion.AutoDetect(connString))
+           .LogTo(Console.WriteLine, LogLevel.Information)
+           .EnableSensitiveDataLogging()
+           .EnableDetailedErrors()
+);
+var app = builder.Build();
+
+var urlBase = app.Configuration["urlBase"];
+
+app.Run();
